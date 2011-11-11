b4a62f1adef3fb3349e6a4c79a027409a640ba8a
Steve Dierker <kiwi@obstkiste.org>
2011-11-11 21:23:05 +0000

    deleted references and constructors for history and statistic from OrderedListPolygon

--- project/src/polygonsSWP/data/OrderedListPolygon.java
+++ project/src/polygonsSWP/data/OrderedListPolygon.java
@@ -7,54 +7,32 @@ import polygonsSWP.util.MathUtils;
 
 
 /**
- * Implementation of Polygon using ordered list. 
- * The assumption is, that the list of polygons is ordered in order 
- * of appearance. So the polygon is drawn from point one to point two to 
- * .. to point n to point 1. So be sure your self added point list meets 
- * the assumption otherwise it won't work.
+ * Implementation of Polygon using ordered list. The assumption is, that the
+ * list of polygons is ordered in order of appearance. So the polygon is drawn
+ * from point one to point two to .. to point n to point 1. So be sure your self
+ * added point list meets the assumption otherwise it won't work.
  * 
  * @author bigzed
  */
-public class OrderedListPolygon implements Polygon
+public class OrderedListPolygon
+  implements Polygon
 {
   List<Point> _coords = new ArrayList<Point>();
-  PStatistics _stats = null;
 
   /**
    * Generates an empty polygon object which will contain no statistics or
    * history.
    */
   public OrderedListPolygon() {
-    this(new ArrayList<Point>(), null);
+    this(new ArrayList<Point>());
   }
 
   /**
-   * Generates an polygon object which consists of the given points,
-   * carrying no statistics object.
+   * Generates an polygon object which consists of the given points, carrying no
+   * statistics object.
    */
   public OrderedListPolygon(List<Point> coords) {
-    this(coords, null);
-  }
-  
-  /**
-   * Generates an empty polygons object which will contain its statistics.
-   * 
-   * @param stats The statistics object which shall be used.
-   */
-  public OrderedListPolygon(PStatistics stats) {
-    this(new ArrayList<Point>(), stats);
-  }
-  
-  /**
-   * Generates an empty polygons object which will contain its statistics
-   * and the given list of points.
-   * 
-   * @param stats The statistics object the Polygon should carry
-   * @param points The list of points the polygon consists of
-   */
-  public OrderedListPolygon(List<Point> coords, PStatistics stats) {
     _coords = coords;
-    _stats = stats;
   }
 
   public List<Point> getPoints() {
@@ -67,15 +45,6 @@ public class OrderedListPolygon implements Polygon
   public void setPoints(List<Point> coords) {
     _coords = coords;
   }
-
-  /**
-   * @return Returns the statistics object associated. If none is present it
-   *         returns null.
-   */
-  public PStatistics getStatictics() {
-    return _stats;
-  }
-
   /**
    * Adds a point to the end of the list.
    * 
@@ -103,46 +72,41 @@ public class OrderedListPolygon implements Polygon
   public void deletePoint(Point p) {
     _coords.remove(p);
   }
-  
+
   public void permute() {
     _coords = MathUtils.permute(_coords);
   }
 
   /**
-   * Determines whether a given ordered list of points
-   * forms a simple polygon.
+   * Determines whether a given ordered list of points forms a simple polygon.
    * 
    * @return true, if the polygon is simple, otherwise false.
    */
   public boolean isSimple() {
-    
-    /* Remark: The approach used here is very naive. As
-     * Held writes in his paper, "The simplicity test for a polygon
-     * is not done in linear time; rather we implemented a
-     * straightforward quadratic approach. (As we will see later this
-     * has no influence on the test results.)", I also decided to let
-     * alone [Cha91] for now and simply check for crossing lines.
-     * 
-     * However, since we're going to implement polygon triangulation
-     * anyway, we should definitely come back here later and improve this.
-     * 
-     * Maybe, instead of the proposed [Cha91], we could also use
-     * the Bentley-Ottmann algorithm, see
-     * http://en.wikipedia.org/wiki/Bentley%E2%80%93Ottmann_algorithm
-     * for explanation.
+
+    /*
+     * Remark: The approach used here is very naive. As Held writes in his
+     * paper, "The simplicity test for a polygon is not done in linear time;
+     * rather we implemented a straightforward quadratic approach. (As we will
+     * see later this has no influence on the test results.)", I also decided to
+     * let alone [Cha91] for now and simply check for crossing lines. However,
+     * since we're going to implement polygon triangulation anyway, we should
+     * definitely come back here later and improve this. Maybe, instead of the
+     * proposed [Cha91], we could also use the Bentley-Ottmann algorithm, see
+     * http://en.wikipedia.org/wiki/Bentley%E2%80%93Ottmann_algorithm for
+     * explanation.
      */
-    
+
     int size = _coords.size();
-    for(int i = 0; i < size - 1; i++) {
+    for (int i = 0; i < size - 1; i++) {
       Edge a = new Edge(_coords.get(i), _coords.get(i + 1));
-      for(int j = i + 1; j < size; j++) {
+      for (int j = i + 1; j < size; j++) {
         Edge b = new Edge(_coords.get(j), _coords.get((j + 1) % size));
-        
-        if(a.isIntersecting(b))
-          return false;
+
+        if (a.isIntersecting(b)) return false;
       }
     }
-    
+
     return true;
   }
 
