128e86d350220b92e4c771b257ffdc6d8c02d1c8
Malte Rohde <malte.rohde@gmx.de>
2011-12-01 21:45:52 +0100

    Polygon: Added toSVG() method.

--- project/src/polygonsSWP/geometry/Polygon.java
+++ project/src/polygonsSWP/geometry/Polygon.java
@@ -41,4 +41,23 @@ public abstract class Polygon
    * @return a random point in the polygon area (including on the edges).
    */
   public abstract Point createRandomPoint();
+  
+  public String toSVG() {
+    StringBuilder sb = new StringBuilder();  
+    List<Point> points = getPoints();
+    
+    sb.append("<?xml version=\"1.0\"?>\n");
+    sb.append("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n");
+    sb.append("<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">\n");
+    sb.append("<polygon points=\"");
+    for(Point p : points) {
+      sb.append(p.x);
+      sb.append(",");
+      sb.append(p.y);
+      sb.append(" ");
+    }
+    sb.append("\" style=\"fill:lime;stroke:purple;stroke-width:1\" />\n");
+    sb.append("</svg>\n");
+    return sb.toString();
+  }
 }
