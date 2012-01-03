0451b09b95c5183f70d4ff1d8677eff00aa95cf1
Steve Dierker <dierker.steve@fu-berlin.de>
2012-01-03 12:25:53 +0000

    History and Scene: * added comments

--- project/src/polygonsSWP/data/History.java
+++ project/src/polygonsSWP/data/History.java
@@ -3,14 +3,40 @@ package polygonsSWP.data;
 import java.util.Iterator;
 import java.util.List;
 
-public interface History {
 
+/**
+ * @author Steve Dierker <dierker.steve@fu-berlin.de>
+ */
+
+public interface History
+{
+
+  /**
+   * This method creates a new scene and returns it.
+   * 
+   * @return the new scene
+   */
   public Scene newScene();
-  
+
+  /**
+   * This adds the scene to the history object.
+   * 
+   * @param newScene the new scene
+   */
   public void addScene(Scene newScene);
-  
+
+  /**
+   * Returns a ordered list of all scenes
+   * 
+   * @return ordered list of scenes
+   */
   public List<Scene> getScenes();
-  
+
+  /**
+   * Returns an iterator for all the scenes in the history object
+   * 
+   * @return
+   */
   public Iterator<Scene> getSceneIterator();
-  
+
 }
