7c9772315249ceb7fc0dd2e225913aa64cc68ecb
Kadir <Kadir.tugan@gmail.com>
2012-01-15 02:03:11 +0100

    added some new attributes to Polygonstatistics

--- src/main/java/polygonsSWP/data/PolygonStatistics.java
+++ src/main/java/polygonsSWP/data/PolygonStatistics.java
@@ -1,15 +1,34 @@
 package polygonsSWP.data;
 
 /**
- * First scratch of needed data. This is NOT(!) the final implementation.
+ * Second scratch of needed data. This may be the final implementation.
  * 
  * @author bigzed
+ * @author Kadir
  */
 public class PolygonStatistics
 {
-  public int number_of_points;
-  public double flaecheninhalt;
-  public double umfang;
-  public double time_for_creating_polygon;
-  public String used_alogirthm;
+  //All
+  public Integer number_of_points = null;
+  public Double surface_area = null;
+  public Double circumference = null; //umfang
+  public String used_algorithm = null;
+  public Long timestamp = null;//System.currentTimeMillis();, use at start of algorithm
+  public Double time_for_creating_polygon = null; //in Millisecs
+  
+  //Permute & Reject + IC&BT + Virmani  
+  public Integer iterations = null;
+ 
+  //RPA
+  //nothing yet
+  
+  
+  //IC&BT
+  public Integer count_of_backtracks = null;
+  
+  //Velocity Virmani
+  public Integer virmani_collisions = null;
+  public Double radius = null;
+  public Double avg_velocity_without_collisions = null;
+  
 }
