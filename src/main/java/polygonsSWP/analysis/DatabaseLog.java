5d2303158f70f3de5aecd4a15686b7e449782b89
Malte Rohde <malte.rohde@gmx.de>
2012-08-16 17:21:04 +0200

    DatabaseLog: Only bind values if not null.

--- src/main/java/polygonsSWP/analysis/DatabaseLog.java
+++ src/main/java/polygonsSWP/analysis/DatabaseLog.java
@@ -16,65 +16,66 @@ public class DatabaseLog
 {
   private SQLiteConnection con;
   private SQLiteQueue queue;
-  
+
   private final String sql =
       "INSERT INTO Statistic "
           + "(id, used_algorithm, number_of_points, surface_area, circumference, timestamp, time_for_creating_polygon, "
           + "iterations, rejections, count_of_backtracks, radius, avg_velocity_without_collisions, initializeRejections, maximumRejections) "
-          + "VALUES(null,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)";
-  
-  
-  public DatabaseLog(String database) throws Exception {
+          + "VALUES(null,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+
+  public DatabaseLog(String database)
+    throws Exception {
     File db = new File(database);
     boolean create_table = !db.exists();
-    
+
     con = new SQLiteConnection(db);
     con.open();
-    
-    if(create_table) {
-      con.exec("CREATE TABLE Statistic (" +
-          "id integer primary key autoincrement," +
-          "used_algorithm integer," +
-          "number_of_points integer," +
-          "surface_area decimal," +
-          "circumference decimal," +
-          "timestamp integer," +
-          "time_for_creating_polygon," +
-          "iterations integer," +
-          "rejections integer," +
-          "count_of_backtracks integer," +
-          "radius decimal," +
-          "avg_velocity_without_collisions decimal," +
-          "initializeRejections integer," +
-          "maximumRejections integer" +
-          ")");
+
+    if (create_table) {
+      con.exec("CREATE TABLE Statistic ("
+          + "id integer primary key autoincrement," + "used_algorithm integer,"
+          + "number_of_points integer," + "surface_area decimal,"
+          + "circumference decimal," + "timestamp integer,"
+          + "time_for_creating_polygon," + "iterations integer,"
+          + "rejections integer," + "count_of_backtracks integer,"
+          + "radius decimal," + "avg_velocity_without_collisions decimal,"
+          + "initializeRejections integer," + "maximumRejections integer" + ")");
     }
-    
+
     queue = new SQLiteQueue(db);
     queue.start();
   }
-  
+
   @Override
   public void writeOut(Polygon polygon, final PolygonStatistics stats) {
-    synchronized(queue) {
+    synchronized (queue) {
       queue.execute(new SQLiteJob<Object>() {
         @Override
-        protected Object job(SQLiteConnection con) throws Throwable {
+        protected Object job(SQLiteConnection con)
+          throws Throwable {
           SQLiteStatement stmt = con.prepare(sql);
-          stmt.bind(0, stats.used_algorithm);
-          stmt.bind(1, stats.number_of_points);
-          stmt.bind(2, stats.surface_area);
-          stmt.bind(3, stats.circumference);
-          stmt.bind(4, stats.timestamp);
-          stmt.bind(5, stats.time_for_creating_polygon);
-          stmt.bind(6, stats.iterations);
-          stmt.bind(7, stats.rejections);
-          stmt.bind(8, stats.count_of_backtracks);
-          stmt.bind(9, stats.radius);
-          stmt.bind(10, stats.avg_velocity_without_collisions);
-          stmt.bind(11, stats.initializeRejections);
-          stmt.bind(12, stats.maximumRejections);
-          stmt.stepThrough();          
+          stmt.bind(1, stats.used_algorithm);
+          stmt.bind(2, stats.number_of_points);
+          stmt.bind(3, stats.surface_area);
+          stmt.bind(4, stats.circumference);
+          stmt.bind(5, stats.timestamp);
+          stmt.bind(6, stats.time_for_creating_polygon);
+          if (stats.iterations != null) 
+            stmt.bind(7, stats.iterations);
+          if (stats.rejections != null) 
+            stmt.bind(8, stats.rejections);
+          if (stats.count_of_backtracks != null)
+            stmt.bind(9, stats.count_of_backtracks);
+          if (stats.radius != null) 
+            stmt.bind(10, stats.radius);
+          if (stats.avg_velocity_without_collisions != null)
+            stmt.bind(11, stats.avg_velocity_without_collisions);
+          if (stats.initializeRejections != null)
+            stmt.bind(12, stats.initializeRejections);
+          if (stats.maximumRejections != null)
+            stmt.bind(13, stats.maximumRejections);
+          
+          stmt.stepThrough();
           return null;
         }
       });
@@ -89,6 +90,6 @@ public class DatabaseLog
     catch (InterruptedException e) {
       // Ignore.
     }
-    con.dispose();    
+    con.dispose();
   }
 }
